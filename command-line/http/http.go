09501725765835d378e33ae424009daeb5ac0a75
tzivia-rot <124471968+tzivia-rot@users.noreply.github.com>
2024-11-07 20:43:54 +0200

    Update http.go

--- command-line/http/http.go
+++ command-line/http/http.go
@@ -9,19 +9,6 @@ import (
 	"net/http"
 )
 
-// type Example struct {
-// 	Inputs []string `json:"input"`
-// 	Output string   `json:"output"`
-// }
-
-// type Exercise struct {
-// 	ID             string    `json:"id" bson:"_id,omitempty"`
-// 	Name           string    `json:"name" bson:"name"`
-// 	Description    string    `json:"description" bson:"description"`
-// 	Examples       []Example `json:"examples"`
-// 	BasisOperation string    `json:"basisOperation"`
-// }
-
 var url = "http://localhost:8080/exercises"
 
 func AddExercise(exercise modules.Exercise) (string, error) {
